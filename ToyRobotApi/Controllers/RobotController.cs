09e2cf82e1472308e22bcbb652121ccd47c0794a
Valerie Yang <598996706yang@gmail.com>
2025-03-03 23:11:58 +0800

    init  ToyRotAPI project, set up sln, Models, Controllers, Tests

---
+++ ToyRobotApi/Controllers/RobotController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToyRobotApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RobotController : ControllerBase
+    {
+        
+    }
+}
