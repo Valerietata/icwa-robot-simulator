de839ccfd4fc678d14be17ee4f9cc0995e2370b7
Valerie Yang <598996706yang@gmail.com>
2025-03-04 01:22:50 +0800

    add rotation and movement api

--- ToyRobotApi/Controllers/RobotController.cs
+++ ToyRobotApi/Controllers/RobotController.cs
@@ -22,16 +22,36 @@ namespace ToyRobotApi.Controllers
             _robot.Place(request.X, request.Y, request.Facing);
             return Ok(new { message = "Robot placed", state = _robot.Report() });
         }
-
-        // Send request for the PLACE command
-        public class PlaceRequest
+        // POST: api/robot/move
+        [HttpPost("move")]
+        public IActionResult Move()
         {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public string Facing { get; set; }
+            _robot.Move();
+            return Ok(new { message = "Robot moved", state = _robot.Report() });
         }
 
-        
+        // POST: api/robot/left
+        [HttpPost("left")]
+        public IActionResult Left()
+        {
+            _robot.Left();
+            return Ok(new { message = "Robot turned left", state = _robot.Report() });
+        }
+
+        // POST: api/robot/right
+        [HttpPost("right")]
+        public IActionResult Right()
+        {
+            _robot.Right();
+            return Ok(new { message = "Robot turned right", state = _robot.Report() });
+        }
+
+        // GET: api/robot/report
+        [HttpGet("report")]
+        public IActionResult Report()
+        {
+            return Ok(new { state = _robot.Report() });
+        }
     }
     // DTO for the PLACE function
     public class PlaceRequest
